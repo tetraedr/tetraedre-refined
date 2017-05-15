831946fbd15389e0e78138190df68650f5512a4f
Seekdesign <martinretrou@hotmail.fr>
2017-05-15 09:08:51 +0200

    style: add img to skills section

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,10 +1,38 @@
-$('.tile').on('mouseover', function() {
-    $('.tile').removeClass('project-open');
-    $('.tile').removeClass('project-min');
-    $('.tile').find('.content').addClass('project-min');
+$('.mutualab').on('mouseover', function() {
+    $('.container-mutualab').removeClass('hidden');
+    $('.container-mutualab').addClass('visible');
 
-    $(this).removeClass('project-min');
-    $(this).addClass('project-open');
-    $(this).find('.content').addClass('visible');
+    $('.container-norauto').addClass('hidden');
+    $('.container-freelance').addClass('hidden');
+    $('.container-optimizme').addClass('hidden');
+});
+
+
+$('.norauto').on('mouseover', function() {
+    $('.container-norauto').removeClass('hidden');
+    $('.container-norauto').addClass('visible');
+
+    $('.container-mutualab').addClass('hidden');
+    $('.container-freelance').addClass('hidden');
+    $('.container-optimizme').addClass('hidden');
+});
+
+$('.freelance').on('mouseover', function() {
+    $('.container-freelance').removeClass('hidden');
+    $('.container-freelance').addClass('visible');
+    
+    $('.container-mutualab').addClass('hidden');
+    $('.container-norauto').addClass('hidden');
+    $('.container-optimizme').addClass('hidden');
+});
+
+
+$('.optimizme').on('mouseover', function() {
+    $('.container-optimizme').removeClass('hidden');
+    $('.container-optimizme').addClass('visible');
+    
+    $('.container-mutualab').addClass('hidden');
+    $('.container-norauto').addClass('hidden');
+    $('.container-freelance').addClass('hidden');
 });
 

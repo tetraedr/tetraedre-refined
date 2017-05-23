6453f7f5908d223fc75719fcbdbb1b7d2908043c
Seekdesign <martinretrou@hotmail.fr>
2017-05-23 11:12:34 +0200

    feat: add projects pages

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -16,3 +16,20 @@ $(window).scroll(function(){
     }
 
 });
+
+// Fade element on scroll
+
+$(window).scroll(function() {
+    $(".back-to-top").css("opacity", 0 + $(window).scrollTop() / 1000);
+});
+
+//Slow scroll vvv
+
+$(document).ready(function() {
+    $('.slow_scroll').on('click', function() { 
+        var page = $(this).attr('href'); 
+        var speed = 750; 
+        $('html, body').animate({ scrollTop: $(page).offset().top }, speed); // Go
+        return false;
+    });
+});

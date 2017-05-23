2b99cae07f79f7e2201009a8e7b3695a285bd1bf
Seekdesign <martinretrou@hotmail.fr>
2017-05-23 12:17:46 +0200

    chore: delete unused bit of js

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,22 +1,3 @@
-/*$elem1 = $('.container-hero-img');
-$elem2 = $('.hero-img');
-var scrollState = 'top';
-
-$(window).scroll(function(){ 
-    
-    var scrollPos = $(window).scrollTop();
-
-    if( ( scrollPos != 0 ) && ( scrollState === 'top' ) ) {
-        $elem2.stop().animate({height: '20vh'}, 300);
-        scrollState = 'scrolled';
-    }       
-    else if( ( scrollPos === 0 ) && ( scrollState === 'scrolled' ) ) {
-        $elem2.stop().animate({height: '90vh'}, 300);
-        scrollState = 'top';
-    }
-
-});*/
-
 // Fade element on scroll
 
 $(window).scroll(function() {

043584bc1703fa1f52fde0ed804c3154f5b8e72e
Seekdesign <martinretrou@hotmail.fr>
2017-05-30 16:08:58 +0200

    chore: update projects image

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
     $('.slow_scroll').on('click', function() { 
         var page = $(this).attr('href'); 
         var speed = 750; 
-        $('html, body').animate({ scrollTop: $(page).offset().top }, speed); // Go
+        $('html, body').animate({ scrollTop: $(page).offset().top }, speed);
         return false;
     });
 });

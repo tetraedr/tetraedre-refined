f1c895236c94b06d90b01f776e1b8d1973767e82
Seekdesign <martinretrou@hotmail.fr>
2017-05-30 10:09:59 +0200

    chore: try other interaction

---
+++ assets/js/halftone.js
@@ -0,0 +1,8 @@
+/*$(document).ready(function(){
+  for( i=0; i<1200; i++) {
+    $('<div class="dot dot-1"></div>').appendTo( '.block-1' );  
+    $('<div class="dot dot-2"></div>').appendTo( '.block-2' );
+    $('<div class="dot dot-3"></div>').appendTo( '.block-3' );
+  };
+});*/
+
